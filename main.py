ec4243e30c987c779eb15a83a581cbf3bfd0d2dc
Oscar <61625502+thisisoscar@users.noreply.github.com>
2023-10-06 15:33:18 +0100

    added title and borders

--- main.py
+++ main.py
@@ -2,10 +2,55 @@ import pygame as pg
 
 pg.init()
 screen = pg.display.set_mode((0,0), pg.FULLSCREEN)
+screen_width = screen.get_width()
+screen_height = screen.get_height()
 clock = pg.time.Clock()
-font = pg.font.SysFont('nunito', 15) # try kokonor font as well as notosansinscriptionalparthian snd notoserifnyiakengpuachuehmong
+title_font = pg.font.SysFont('arial', 40) # nunito kokonor notosansinscriptionalparthian notoserifnyiakengpuachuehmong
+big_border_width = 3
+
+def draw_section_titles():
+    n_sections = len(kanban_data)
+    
+    text_surfaces = [title_font.render(i, True, (255,255,255)) for i in list(kanban_data)]
+    
+    text_distances = screen_width // (n_sections)
+
+    x_pos = 0
+
+    for surface in text_surfaces:
+        x_pos += text_distances
+
+        text_width = surface.get_width()
+
+        adjustment = text_distances // 2
+
+        text_rect = surface.get_rect()
+        text_rect.midtop = (x_pos-adjustment, 0)
+
+        screen.blit(surface, text_rect)
+    
+    return text_surfaces
 
 
+def draw_vertical_borders():
+    n_sections = len(kanban_data)
+
+    border_distances = screen_width // n_sections
+
+    x_pos = 0
+
+    for _ in range(n_sections-1):
+        x_pos += border_distances
+
+        pg.draw.line(screen, (255,255,255), (x_pos,0), (x_pos,screen_height), big_border_width)
+
+def draw_horizontal_border(text_surfaces):
+    max_height = max([i.get_height() for i in text_surfaces])
+    
+    pg.draw.line(screen, (255,255,255), (0,max_height), (screen_width,max_height), big_border_width)
+
+
+kanban_data = {'to do':[], 'in progess':[], 'done':[]}
 
 running = True
 while running:
@@ -14,7 +59,9 @@ while running:
             pg.quit()
             quit()
     
-
+    text_surfaces = draw_section_titles()
+    draw_vertical_borders()
+    draw_horizontal_border(text_surfaces)
 
     clock.tick(60)
     pg.display.flip()
